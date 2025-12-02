a658c75116abf3d232260307b7eb95619b9146a9
Espadas <joseespadas995@gmail.com>
2025-12-02 12:19:55 +0100

    fix: reorganizado estructura proyecto added: RandomGeneratorTest.java

--- src/main/java/es/dm2e/psp/actividad01/grupo2/Main.java
+++ src/main/java/es/dm2e/psp/actividad01/grupo2/Main.java
@@ -1,7 +1,9 @@
 package es.dm2e.psp.actividad01.grupo2;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("HI");
     }
 }
---
+++ src/test/java/es/dm2e/psp/actividad01/grupo2/RandomGeneratorTest.java
@@ -0,0 +1,17 @@
+package es.dm2e.psp.actividad01.grupo2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomGeneratorTest {
+
+    @Test
+    void randomInt() {
+        System.out.println("test");
+    }
+
+    @Test
+    void randomFloat() {
+    }
+}
