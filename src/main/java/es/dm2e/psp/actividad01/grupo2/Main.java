8b24ddf2e6e8a42852de129275500ade6cb83128
Espadas <joseespadas995@gmail.com>
2025-12-03 10:46:22 +0100

    added: input de hilos a usar added: arrancar procesador de transferencias

--- src/main/java/es/dm2e/psp/actividad01/grupo2/Main.java
+++ src/main/java/es/dm2e/psp/actividad01/grupo2/Main.java
@@ -1,6 +1,8 @@
 package es.dm2e.psp.actividad01.grupo2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -84,7 +86,7 @@ public class Main {
         do {
             try {
                 nTransferencias = Integer.parseInt(transferencias);
-                if (nTransferencias <= 0){
+                if (nTransferencias <= 0) {
                     System.out.println("El número de transferencias no pueden ser menor o igual a 0\nIntroduzca un nuevo número:");
                     transferencias = SCANNER.nextLine().trim();
                 } else {
@@ -97,6 +99,34 @@ public class Main {
         } while (!nTransferenciasValido);
 
         // ======================================================
+        // =         INPUT DE USUARIO: NÚMERO DE HILOS          =
+        // ======================================================
+        String preguntaHilos = """
+                 -------------------------------------
+                | Introduce el número de hilos a usar |
+                 -------------------------------------
+                """;
+        System.out.print(preguntaHilos);
+        String hilos = SCANNER.nextLine().trim();
+        int nHilos = 0;
+        boolean nHilosValido = false;
+
+        do {
+            try {
+                nHilos = Integer.parseInt(transferencias);
+                if (nHilos <= 0) {
+                    System.out.println("El número de hilos no puede ser menor o igual a 0\nIntroduzca un nuevo número:");
+                    hilos = SCANNER.nextLine().trim();
+                } else {
+                    nHilosValido = true;
+                }
+            } catch (NumberFormatException e) {
+                System.out.printf("<%s> no es un número\nIntroduzca un nuevo número:\n", hilos);
+                hilos = SCANNER.nextLine().trim();
+            }
+        } while (!nHilosValido);
+
+        // ======================================================
         // =     ARRANCAR PROCESO GENERADOR TRANSFERENCIAS      =
         // ======================================================
         ProcessBuilder procesoGenerador = new ProcessBuilder("java", "es.dm2e.psp.actividad01.grupo2.programas.GeneradorFichero");
@@ -106,10 +136,36 @@ public class Main {
         try {
             Process process = procesoGenerador.start();
 
-            try(PrintWriter pw = new PrintWriter(process.getOutputStream())) {
+            try (PrintWriter pw = new PrintWriter(process.getOutputStream())) {
                 pw.println(directorio);
                 pw.println(fichero);
                 pw.println(nTransferencias);
+                pw.flush();
+            }
+
+            int exitValue = process.waitFor();
+            System.out.println(exitValue);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // ======================================================
+        // =     ARRANCAR PROCESO PROCESADOR TRANSFERENCIAS     =
+        // ======================================================
+        ProcessBuilder procesoProcesador = new ProcessBuilder("java", "es.dm2e.psp.actividad01.grupo2.programas.ProcesadorFichero");
+        procesoGenerador.environment().put("JAVA_HOME", System.getProperty("java.home"));
+        procesoGenerador.environment().put("CLASSPATH", System.getProperty("java.class.path"));
+
+        try {
+            Process process = procesoProcesador.start();
+
+            try (PrintWriter pw = new PrintWriter(process.getOutputStream());
+                 BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                pw.println(directorio);
+                pw.println(fichero);
+                pw.println(nTransferencias);
+                pw.println(nHilos);
                 pw.flush();
             }
 
